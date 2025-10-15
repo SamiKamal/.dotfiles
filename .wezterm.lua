755229b940bdb3dceff53de1fda15044954c5021
Sami Ibrahim <sami-dev@hotmail.com>
2025-10-15 21:27:09 +0300

    chore: formatting

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,7 +12,9 @@ local act = wezterm.action
 
 local config = {}
 -- Use config builder object if possible
-if wezterm.config_builder then config = wezterm.config_builder() end
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
 
 -- Settings
 -- config.default_prog = {'C:\\Users\\Sami\\.oh-my-zsh\\lib'}
@@ -33,13 +35,13 @@ config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
 config.scrollback_lines = 3000
 
-config.initial_cols = 120;
-config.initial_rows = 26;
+config.initial_cols = 120
+config.initial_rows = 26
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
-  saturation = 0.24,
-  brightness = 0.5
+    saturation = 0.24,
+    brightness = 0.5,
 }
 
 -- Keys
@@ -97,30 +99,30 @@ config.keys = {
 }
 -- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
 for i = 1, 9 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = "LEADER",
-    action = act.ActivateTab(i - 1)
-  })
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "LEADER",
+        action = act.ActivateTab(i - 1),
+    })
 end
 
 config.key_tables = {
-  resize_pane = {
-    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
-    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
-    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
-    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
-    { key = "Escape", action = "PopKeyTable" },
-    { key = "Enter",  action = "PopKeyTable" },
-  },
-  move_tab = {
-    { key = "h",      action = act.MoveTabRelative(-1) },
-    { key = "j",      action = act.MoveTabRelative(-1) },
-    { key = "k",      action = act.MoveTabRelative(1) },
-    { key = "l",      action = act.MoveTabRelative(1) },
-    { key = "Escape", action = "PopKeyTable" },
-    { key = "Enter",  action = "PopKeyTable" },
-  }
+    resize_pane = {
+        { key = "h",      action = act.AdjustPaneSize({ "Left", 1 }) },
+        { key = "j",      action = act.AdjustPaneSize({ "Down", 1 }) },
+        { key = "k",      action = act.AdjustPaneSize({ "Up", 1 }) },
+        { key = "l",      action = act.AdjustPaneSize({ "Right", 1 }) },
+        { key = "Escape", action = "PopKeyTable" },
+        { key = "Enter",  action = "PopKeyTable" },
+    },
+    move_tab = {
+        { key = "h",      action = act.MoveTabRelative(-1) },
+        { key = "j",      action = act.MoveTabRelative(-1) },
+        { key = "k",      action = act.MoveTabRelative(1) },
+        { key = "l",      action = act.MoveTabRelative(1) },
+        { key = "Escape", action = "PopKeyTable" },
+        { key = "Enter",  action = "PopKeyTable" },
+    },
 }
 
 -- Tab bar
@@ -129,56 +131,58 @@ config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
 config.tab_bar_at_bottom = false
 wezterm.on("update-status", function(window, pane)
-  -- Workspace name
-  local stat = window:active_workspace()
-  local stat_color = "#f7768e"
-  -- It's a little silly to have workspace name all the time
-  -- Utilize this to display LDR or current key table name
-  if window:active_key_table() then
-    stat = window:active_key_table()
-    stat_color = "#7dcfff"
-  end
-  if window:leader_is_active() then
-    stat = "LDR"
-    stat_color = "#bb9af7"
-  end
+    -- Workspace name
+    local stat = window:active_workspace()
+    local stat_color = "#f7768e"
+    -- It's a little silly to have workspace name all the time
+    -- Utilize this to display LDR or current key table name
+    if window:active_key_table() then
+        stat = window:active_key_table()
+        stat_color = "#7dcfff"
+    end
+    if window:leader_is_active() then
+        stat = "LDR"
+        stat_color = "#bb9af7"
+    end
 
-  -- Current working directory
-  local basename = function(s)
-    -- Nothing a little regex can't fix
-    return string.gsub(s, "(.*[/\\])(.*)", "%2")
-  end
-  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
-  local cwd = pane:get_current_working_dir()
-  cwd = cwd and basename(cwd) or ""
-  -- Current command
-  local cmd = pane:get_foreground_process_name()
-  cmd = cmd and basename(cmd) or ""
+    -- Current working directory
+    local basename = function(s)
+        if not s then return "" end
+        s = tostring(s) -- handle wezterm.Url, userdata, etc.
 
-  -- Time
-  local time = wezterm.strftime("%H:%M")
+        return s:match("([^/\\]+)$") or s
+    end
+    -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
+    local cwd = pane:get_current_working_dir()
+    cwd = cwd and basename(cwd) or ""
+    -- Current command
+    local cmd = pane:get_foreground_process_name()
+    cmd = cmd and basename(cmd) or ""
 
-  -- Left status (left of the tab line)
-  window:set_left_status(wezterm.format({
-    { Foreground = { Color = stat_color } },
-    { Text = "  " },
-    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-    { Text = " |" },
-  }))
+    -- Time
+    local time = wezterm.strftime("%H:%M")
 
-  -- Right status
-  window:set_right_status(wezterm.format({
-    -- Wezterm has a built-in nerd fonts
-    -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
-    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-    { Text = " | " },
-    { Foreground = { Color = "#e0af68" } },
-    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-    "ResetAttributes",
-    { Text = " | " },
-    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
-    { Text = "  " },
-  }))
+    -- Left status (left of the tab line)
+    window:set_left_status(wezterm.format({
+        { Foreground = { Color = stat_color } },
+        { Text = "  " },
+        { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+        { Text = " |" },
+    }))
+
+    -- Right status
+    window:set_right_status(wezterm.format({
+        -- Wezterm has a built-in nerd fonts
+        -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+        { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+        { Text = " | " },
+        { Foreground = { Color = "#e0af68" } },
+        { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+        "ResetAttributes",
+        { Text = " | " },
+        { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+        { Text = "  " },
+    }))
 end)
 
 --[[ Appearance setting for when I need to take pretty screenshots
@@ -190,6 +194,5 @@ config.window_padding = {
 
 }
 --]]
-
 
 return config
